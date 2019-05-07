27edfadc9ecf694ef3995a0e035437fe2e18ada6
Pedro Campos <pedrocamp@gmail.com>
2019-05-07 14:42:27 -0300

    changed into package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='zendeskcli',
+      version='0.1',
+      description='client api for zendesk',
+      url='https://github.com/pedrodcampos/zendesk-client',
+      author='Pedro Campos',
+      author_email='pedrocamp@gmail.com ',
+      license='MIT',
+      packages=['zendeskcli'],
+      zip_safe=False)
