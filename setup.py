1e34198100efbd15304cc352a6d42b062097284c
Pedro Campos <pedrocamp@gmail.com>
2019-05-07 14:50:38 -0300

    add subpackages to setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='zendeskcli',
       version='0.1',
@@ -7,5 +7,5 @@ setup(name='zendeskcli',
       author='Pedro Campos',
       author_email='pedrocamp@gmail.com ',
       license='MIT',
-      packages=['zendeskcli'],
+      packages=find_packages(),
       zip_safe=False)
