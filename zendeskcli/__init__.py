8686d316a347389093434413d313904caaf14b5f
Pedro Campos <pedrocamp@gmail.com>
2019-07-03 12:00:56 -0300

    Added initialization parameter to helpcenter function in wrapper class

--- zendeskcli/__init__.py
+++ zendeskcli/__init__.py
@@ -4,6 +4,14 @@ from .support import ZendeskSupport
 
 
 class ZendeskClient:
-    def __init__(self, url, auth, auth_method='token'):
+    def __init__(self, url, auth, auth_method="token"):
+        self.__url = url
+        self.__auth = auth
+        self.__auth_method = auth_method
         self.support = ZendeskSupport(url, auth, auth_method)
-        self.helpcenter = ZendeskHC(url, auth, auth_method)
+        self.helpcenter = self.get_helpcenter
+
+    def get_helpcenter(self, brand_id):
+        brand = self.support.brands(brand_id)["brand"]
+
+        return ZendeskHC(brand["brand_url"], self.__auth, self.__auth_method)
