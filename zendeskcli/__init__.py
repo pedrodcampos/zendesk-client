cb70c93a65dad88efd75a92b1da8fc3fc343c7d6
Pedro Campos <pedrocamp@gmail.com>
2019-07-03 11:42:25 -0300

    Added wrapper class for both support and helpcenter class

--- zendeskcli/__init__.py
+++ zendeskcli/__init__.py
@@ -1,3 +1,9 @@
 from . import oauth
 from .helpcenter import ZendeskHC
 from .support import ZendeskSupport
+
+
+class ZendeskClient:
+    def __init__(self, url, auth, auth_method='token'):
+        self.support = ZendeskSupport(url, auth, auth_method)
+        self.helpcenter = ZendeskHC(url, auth, auth_method)
